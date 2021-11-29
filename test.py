08c059deae8800956e99ab1e89ec41609a74fbd6
bschlottman <73973450+bschlottman@users.noreply.github.com>
2021-11-29 08:34:14 -0700

    updated test.py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 """ Tests for memory.py """
 
-from subprocess import getstatusoutput
+#from subprocess import getstatusoutput
 import os
-import re
+#import re
 
 
 PRG = './memory.py'
@@ -14,13 +14,3 @@ def test_exists():
     """ Program exists """
 
     assert os.path.isfile(PRG)
-
-
-# --------------------------------------------------
-def test_usage():
-    """ Prints usage """
-
-    for flag in ['-h', '--help']:
-        rv, out = getstatusoutput('{} {}'.format(PRG, flag))
-        assert rv == 0
-        assert re.match("usage", out, re.IGNORECASE)
